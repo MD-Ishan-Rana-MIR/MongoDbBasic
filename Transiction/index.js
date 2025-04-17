807baaec331115dce0a66b59ebd8a25d7fd031dd
ishanrana363 <ishanrana363@gmail.com>
2025-04-17 23:33:21 +0600

    session completed #

--- Transiction/index.js
+++ Transiction/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://ishanrana950:Xi9Qot1VD3BjGk31@cluster0.5xmvgk6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -49,7 +49,83 @@ async function run() {
                 })
             }
         });
+        app.post("/transfer-money", async (req, res) => {
+            console.log(req.body)
+            const { formAccountNumber, toAccountNumber, amount } = req.body;
+            if (!formAccountNumber || !toAccountNumber || !amount) {
+                return res.status(400).json({
+                    status: "fail",
+                    msg: "form acctount number to account number and amount missing"
+                });
+            }
+            const session = client.startSession();
 
+            try {
+                await session.withTransaction(async () => {
+                    // sender account money withdraw
+                    const withdrawResult = await accountCollection.updateOne(
+                        {
+                            _id: new ObjectId(formAccountNumber),
+                            amount: { $gte: amount }
+                        },
+                        {
+                            $inc: { amount: - amount }
+                        },
+                        {
+                            session: session
+                        }
+                    );
+                    if (withdrawResult.modifiedCount !== 1) {
+                        return res.status(400).json({
+                            status: "fail",
+                            msg: "Insufficient balance. Transiction fail"
+                        });
+                    }
+                    // step 2 
+                    await accountCollection.updateOne(
+                        {
+                            _id: new ObjectId(formAccountNumber)
+                        },
+                        {
+                            $inc: {
+                                amount: amount
+                            }
+                        },
+                        {
+                            session: session
+                        }
+                    );
+
+                    // step 3 
+
+                    transictionsCollection.insertOne(
+                        {
+                            form: new ObjectId(),
+                            to: new ObjectId(toAccountNumber),
+                            amount,
+                            timestamps: new Date()
+                        },{
+                            session : session
+                        }
+                    )
+
+
+                });
+                return res.status(200).json({
+                    status : 'success',
+                    msg : "Transiction successfully"
+                })
+            } catch (error) {
+                return res.status(500).json({
+                    status: "fail",
+                    msg: "Something went wrong",
+                    error: error
+                });
+            }finally{
+                session.endSession();
+            }
+
+        });
 
 
 
